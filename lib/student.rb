71362ca69e00e9cebde01537aa8953796f10493f
Warren Harper II <warrenharper2@att.net>
2019-10-01 16:55:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,6 @@
 class Student
-  attr_accessor :id, :name, :grade
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
